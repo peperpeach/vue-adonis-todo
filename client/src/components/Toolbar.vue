7d08a656c9ff56e7edabb489aca85f7d6f40c32e
yada <layada@bigc.co.th>
2019-04-22 16:38:43 +0700

    Client - Logout

--- client/src/components/Toolbar.vue
+++ client/src/components/Toolbar.vue
@@ -17,7 +17,7 @@
         <v-icon class="mr-2">fingerprint</v-icon>
         Login
       </v-btn>
-      <v-btn flat v-if="isLoggedIn">
+      <v-btn flat v-if="isLoggedIn" @click="logout">
         <v-icon class="mr-2">exit_to_app</v-icon>
         Logout
       </v-btn>
@@ -29,12 +29,17 @@
   </v-toolbar>
 </template>
 <script>
-import { mapGetters } from 'vuex';
+import { mapGetters, mapActions } from 'vuex';
 
 export default {
   computed: {
     ...mapGetters('authentication', [
       'isLoggedIn',
+    ]),
+  },
+  methods: {
+    ...mapActions('authentication', [
+      'logout',
     ]),
   },
 };
