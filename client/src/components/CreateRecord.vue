e451ee8cd97b9d51c49bca52114f53451f445201
yada <layada@bigc.co.th>
2019-04-23 13:54:51 +0700

    Client - Function Projects

---
+++ client/src/components/CreateRecord.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-layout row wrap class="mt-4">
+    <v-flex xs-9>
+      <v-text-field
+        :placeholder="placeholder"
+        :value="value"
+        @input="$emit('onInput', $event)"
+        @keyup.enter="$emit('create')"
+      ></v-text-field>
+    </v-flex>
+    <v-flex xs-3 class="text-xs-right">
+      <v-btn
+        dark
+        class="mt-2"
+        color="green"
+        @click="$emit('create')"
+      >
+        <v-icon class="mr-2">add_circle</v-icon>
+        Create
+      </v-btn>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+export default {
+  props: [
+    'placeholder',
+    'value',
+  ],
+};
+</script>
+
+<style>
+
+</style>
